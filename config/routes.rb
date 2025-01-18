7229a04dcea852baea87305332952a29a87a5631
Brian Cary <1062145+briancarycom@users.noreply.github.com>
2025-01-17 20:29:49 -0500

    wrap route in check

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-UrlChecker::Engine.routes.draw do
-  get "/check" => "urls#check", :as => :url_check
+unless UrlChecker::Engine.routes.routes.find { |r| r.name == 'url_check' }
+  UrlChecker::Engine.routes.draw do
+    get "/check" => "urls#check", :as => :url_check
+  end
 end
